dbd2a28ce31bfc85fcd4d6b54c25aefc253d45a7
Alex Westphal <alexelevenfifty@gmail.com>
2023-10-25 14:17:34 -0400

    gear changes

--- GroupApiProject.WebApi/Controllers/GearController.cs
+++ GroupApiProject.WebApi/Controllers/GearController.cs
@@ -1,4 +1,8 @@
+using GroupApiProject.Models.Gear;
+using GroupApiProject.Models.Responses;
 using GroupApiProject.Services.Gear;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GroupApiProject.WebApi.Controllers;
@@ -8,7 +12,7 @@ namespace GroupApiProject.WebApi.Controllers;
 public class GearController : ControllerBase
 {
     private readonly IGearService _gearService;
-  
+
 
     public GearController(IGearService gearService)
     {
@@ -21,4 +25,52 @@ public class GearController : ControllerBase
         var gear = await _gearService.GetGearsAsync();
         return Ok(gear);
     }
+
+    [HttpGet("{gearId:int}")]
+    public async Task<IActionResult> GetGearByAsyncId([FromRoute] int gearId)
+    {
+        var gear = await _gearService.GetGearByIdAsync(gearId);
+        return Ok(gear);
+    }
+    [Authorize]
+    [HttpPost]
+    public async Task<IActionResult> GearCreate([FromBody] GearCreate model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var registerResult = await _gearService.CreateGearAsync(model);
+        {
+            var TextResponse = "Gear is created!";
+            return Ok(TextResponse);
+        }
+
+    }
+    [Authorize]
+    [HttpPut]
+    public async Task<IActionResult> UpdateGearByIdAsync([FromBody] GearEdit request)
+    {
+        if (ModelState.IsValid)
+        {
+            return await _gearService.UpdateGearByIdAsync(request)
+            ? Ok("Gear has been updated.")
+            : BadRequest("Gear could not be updated. :()");
+        }
+        return BadRequest(ModelState);
+
+    }
+
+
+    [Authorize]
+    [HttpDelete]
+    public async Task<IActionResult> DeleteGearAsync([FromBody] GearDelete gearId)
+    {
+        return await _gearService.DeleteGearAsync(gearId)
+        ? Ok($"Gear {gearId} was deleted successfully.")
+        : BadRequest($"Note {gearId} was not deleted.");
+    }
 }
+
+
